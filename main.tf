e412601320e72ab94b326e72c87664ba8f93d881
openthisworld <puchok80@gmail.com>
2023-01-11 17:21:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,33 +2,33 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "example" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "example" {
-  vpc_id            = aws_vpc.example.id
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.subnet_cidr
   availability_zone = var.az
 }
 
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 }
 
-resource "aws_security_group" "example" {
-  name        = "example"
-  description = "example security group"
-  vpc_id      = aws_vpc.example.id
+resource "aws_security_group" "my_security_group" {
+  name        = "my_security_group"
+  description = "my security group"
+  vpc_id      = aws_vpc.my_vpc.id
 }
 
 resource "aws_security_group_rule" "ingress_http" {
@@ -37,7 +37,7 @@ resource "aws_security_group_rule" "ingress_http" {
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
+  security_group_id = aws_security_group.my_security_group.id
 }
 
 resource "aws_security_group_rule" "ingress_ssh" {
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "ingress_ssh" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
+  security_group_id = aws_security_group.my_security_group.id
 }
 
 resource "aws_security_group_rule" "egress_all" {
@@ -55,28 +55,28 @@ resource "aws_security_group_rule" "egress_all" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
+  security_group_id = aws_security_group.my_security_group.id
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "my_instances" {
   ami           = var.ami_id
   instance_type = var.instance_type
   count         = var.instance_count
 
-  vpc_security_group_ids = [aws_security_group.example.id]
-  subnet_id               = aws_subnet.example.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  subnet_id               = aws_subnet.my_subnet.id
 
   tags = {
-    Name = "example"
+    Name = "my_instance"
   }
 }
 
-resource "aws_eip" "example" {
+resource "aws_eip" "my_eip" {
   count       = var.instance_count
   vpc         = true
-  instance_id = aws_instance.example.*.id[count.index]
+  instance_id = aws_instance.my_instances.*.id[count.index]
 }
 
 output "eip" {
-  value = aws_eip.example.*.public_ip
+  value = aws_eip.my_eip.*.public_ip
 }
